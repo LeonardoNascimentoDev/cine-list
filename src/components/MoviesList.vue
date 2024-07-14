9bac5f07cd08d4f6b0bd8111a8193e54eadcda46
Leonardo Nascimento <lsncwb@gmail.com>
2024-07-14 19:56:58 -0300

    feat: creating movie list

---
+++ src/components/MoviesList.vue
@@ -0,0 +1,104 @@
+<template>
+  <div v-if="movies" class="container">
+    <div>
+      <slot></slot>
+    </div>
+    <ul ref="movies" class="movies">
+    </ul>
+  </div>
+</template>
+
+<script>
+import useFetch from "@/composables/fetch";
+import ratingsColor from "@/utils/ratingsColor.js";
+
+export default {
+  name: "MoviesList",
+  components: {
+  },
+
+  props: {
+    path: { type: String, default: "/discover/movie", required: false },
+    genres: { type: String, default: "", required: false },
+    sortBy: { type: String, default: "", required: false },
+  },
+
+  data() {
+    return {
+      movies: null,
+      currentPage: 1,
+    };
+  },
+
+  watch: {
+    currentPage() {
+      this.fetchMovies();
+      this.ratingsColor();
+    },
+  },
+
+  created() {
+    this.fetchMovies();
+  },
+
+  methods: {
+    ratingsColor,
+    async fetchMovies() {
+      const data = await useFetch({
+        path: this.path,
+        query: {
+          page: this.currentPage,
+          language: "pt-BR",
+          with_genres: this.genres,
+          sort_by: this.sortBy,
+        },
+      });
+      this.movies = data;
+      this.ratingsColor();
+    },
+
+    paginate(page) {
+      this.currentPage = page;
+      this.$refs.movies.scrollTo({ behavior: "smooth", left: 0 });
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "../scss/abstracts/_mixins.scss";
+
+.container {
+  padding-left: 30px;
+
+  > div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  @include responsive("medium") {
+    padding-left: 20px;
+    > div {
+      display: initial;
+      position: relative;
+    }
+  }
+}
+
+h1 {
+  @include title;
+  margin-bottom: 15px;
+}
+
+.movies {
+  @include scrollbar(20px, 20px);
+  display: flex;
+  overflow-x: auto;
+  gap: 30px 15px;
+  padding: 20px 20px 20px 0;
+  @include responsive("small") {
+    width: 100vw;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,18 +3,71 @@
     <TheHeader />
 
     <main v-once>
+      <MoviesList path="/movie/now_playing">
+        <h1>Em cartaz</h1>
+      </MoviesList>
+
+      <MoviesList sortBy="popularity.desc">
+        <h1>Mais populares</h1>
+      </MoviesList>
+
+      <MoviesList genres="28,878" sortBy="popularity.desc">
+        <h1>Filmes de ação e ficção científica</h1>
+      </MoviesList>
+
+      <MoviesList genres="35" sortBy="popularity.desc">
+        <h1>Filmes de comédia</h1>
+      </MoviesList>
+
+      <MoviesList genres="16,12" sortBy="vote_count.desc">
+        <h1>Filmes de animação e aventura</h1>
+      </MoviesList>
+
+      <MoviesList genres="10751" sortBy="vote_count.desc">
+        <h1>Para assistir com a família</h1>
+      </MoviesList>
+
+      <MoviesList genres="10749" sortBy="vote_count.desc">
+        <h1>Filmes de romance</h1>
+      </MoviesList>
+
+      <MoviesList genres="18" sortBy="vote_count.desc">
+        <h1>Filmes de drama</h1>
+      </MoviesList>
+
+      <MoviesList genres="10752" sortBy="vote_count.desc">
+        <h1>Filmes de guerra</h1>
+      </MoviesList>
+
+      <MoviesList genres="80" sortBy="vote_count.desc">
+        <h1>Filmes policiais</h1>
+      </MoviesList>
+
+      <MoviesList genres="27" sortBy="vote_count.desc">
+        <h1>Filmes de terror</h1>
+      </MoviesList>
+
+      <MoviesList genres="53" sortBy="vote_count.desc">
+        <h1>Filmes de suspense</h1>
+      </MoviesList>
+
+      <MoviesList genres="99" sortBy="vote_count.desc">
+        <h1>Documentários</h1>
+      </MoviesList>
     </main>
   </div>
 </template>
 
 <script>
 import TheHeader from "@/components/TheHeader.vue";
+import MoviesList from "@/components/MoviesList.vue";
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Home",
   components: {
     TheHeader,
+    MoviesList,
   },
 };
 </script>
